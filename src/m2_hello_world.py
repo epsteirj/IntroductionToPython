daafd49edbd6ef9c5c666db0530558230c8190a6
epsteirj <epsteirj@rose-hulman.edu>
2017-11-27 09:40:01 -0500

    Todo 5

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -13,7 +13,7 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 1.
+# Done: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
 #   Run this module by right clicking anywhere in this window and select
@@ -26,7 +26,7 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a TO DO in this module.
 #
@@ -51,23 +51,25 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 3.
+# Done: 3.
 #   Add another   print   statement below.
 #   It should print any string that you want (but keep it G-rated!)
 #   Test your code by re-running this module using either the right click
 #       method again or by using the play button in the upper right.
 #   Look at the Console to be sure that your string printed as expected.
+print('he is making me write this')
 #
 ########################################################################
 
 ########################################################################
 #
-# TODO: 4.
+# Done: 4.
 #   Add yet another   print   statement.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
 #   You do NOT have to use strings for this, so no quotation marks!
-#
+print(3607*34227)
+
 #   TEST your code by re-running this module, then asking someone
 #   whom you trust:  What number did your   print   display for TO DO 4?
 #   (HINT: It is an INTERESTING number.)  Get help if your value is wrong.

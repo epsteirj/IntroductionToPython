4bf26573347c01a682dca742564bc45891469831
epsteirj <epsteirj@rose-hulman.edu>
2017-11-28 18:55:31 -0500

    Done

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Ryan Epstein
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -20,7 +20,36 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #    2. Each rg.SimpleTurtle object draws something
 #         (by moving, using its rg.Pen).  ANYTHING is fine!
 #    3. Each rg.SimpleTurtle moves inside a LOOP.
-#
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red', 3)
+red_turtle.speed = 10
+
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('blue', 3)
+blue_turtle.speed = 10
+
+for k in range (3):
+    blue_turtle.right(90)
+    blue_turtle.forward(30)
+
+
+for k in range (3):
+    red_turtle.left(90)
+    red_turtle.forward(30)
+
+for k in range (3):
+    red_turtle.left(90)
+    red_turtle.forward(30)
+
+for k in range (3):
+    red_turtle.left(90)
+    red_turtle.forward(30)
+
 #  Be creative!  Strive for way-cool pictures!  Abstract pictures rule!
 #
 #  If you make syntax (notational) errors, no worries -- get help
